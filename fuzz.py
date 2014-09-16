36e48fa8a5ab28d198ef938b4136276c0fb918cb
Cailin <cxl2467@rit.edu>
2014-09-16 02:00:07 -0400

    Setup basic command line args parsing and link crawling

--- fuzz.py
+++ fuzz.py
@@ -1,15 +1,28 @@
-import requests;
-import sys;
+import sys
+import requests
+import bs4
 
 def printErrorMessage():
     print("Please enter a command in the following format:")
     print("python fuzz.py [discover | test] url OPTIONS")    
 
-def discover(url):
+def discover(args):
     print("fuzz round 1 - discover!")
-    print(url);
 
-def test():
+    url = args[2];
+    response = requests.get(url);
+
+    scrapeLinks(response);
+
+def scrapeLinks(response):
+    soup = bs4.BeautifulSoup(response.text);
+    anchors = soup.find_all('a');
+
+    for anchor in anchors:
+        if anchor['href']:
+            print(anchor['href']);
+
+def test(args):
     print("fuzz round 2 - test!")
 
 def main():
@@ -19,6 +32,8 @@ def main():
         printErrorMessage();
 
     if args[1] == "discover":
-        discover(args[2]);
+        discover(args);
+    elif args[1] == "test":
+        test(args);
 
 main();
